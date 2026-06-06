a93214fd04ec60870525f3f597c66177842a240b
Utkarsh-Bhargava <utkarshbhargava947@gmail.com>
2026-06-06 12:12:22 +0530

    Update admin.py

--- backend/TMS_project/main_app/admin.py
+++ backend/TMS_project/main_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Driver)
+admin.site.register(Fuel)
+admin.site.register(Toll)
+admin.site.register(Vehicle)
+admin.site.register(Trip)
+admin.site.register(LRBilty)
+admin.site.register(EWayBill)
+admin.site.register(FinanceTransaction)
+admin.site.register(Vendor)
+admin.site.register(Inventory)
+admin.site.register(Tracking)
